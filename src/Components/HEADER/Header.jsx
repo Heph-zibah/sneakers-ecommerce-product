71312d7901d676d9be1cdf287f4ddb35a76cd27a
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-14 21:56:43 +0100

    created a header folder for the header file

---
+++ src/Components/HEADER/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../../Assets/logo.svg'
+import {FaBars} from 'react-icons/fa'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {links} from './linkData'
+import avatar from '../../Assets/image-avatar.png'
+const Header = () => {
+  return (
+    <>
+     <nav>
+        <div className='nav__logo'>
+            <FaBars className='menu__icon'/>
+            <img src={logo} alt='sneakers logo' />
+        </div>
+        <ul className='nav__links'>
+        {links.map((link) => {
+              const {url, id, text} = link;
+              return <li className='nav__link' key={id}><a href={url}>{text}</a></li>
+            })}
+        </ul>
+        <div className='nav__action'>
+          <AiOutlineShoppingCart className='nav__cart'/>
+          <img src={avatar} alt="avatar"  className='nav__avatar'/>
+        </div>
+     </nav>
+    </>
+  )
+}
+
+export default Header
---
+++ src/Components/HEADER/linkData.js
@@ -0,0 +1,29 @@
+export const links =  [
+        {
+            id:1,
+            url: '/collection',
+            text: 'collection'
+        },
+        {
+            id:2,
+            url: '/men',
+            text: 'men'
+        },
+        {
+            id:3,
+            url: '/women',
+            text: 'women'
+        },
+        {
+            id:4,
+            url: '/contact',
+            text: 'about'
+        },
+        {
+            id:5,
+            url: '/contact',
+            text: 'contact'
+        },
+    ]
+
+
