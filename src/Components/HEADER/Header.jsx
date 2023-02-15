530c0c4a10e2da0be840a4a63b8a6de612c88f67
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-15 15:00:01 +0100

    give the nav head class

--- src/Components/HEADER/Header.jsx
+++ src/Components/HEADER/Header.jsx
@@ -9,16 +9,18 @@ const Header = () => {
   return (
     <>
      <nav>
+       <div className='nav__head'>
         <div className='nav__logo'>
-            <FaBars className='menu__icon'/>
-            <img src={logo} alt='sneakers logo' />
-        </div>
-        <ul className='nav__links'>
-        {links.map((link) => {
-              const {url, id, text} = link;
-              return <li className='nav__link' key={id}><a href={url}>{text}</a></li>
-            })}
-        </ul>
+              <FaBars className='menu__icon'/>
+              <img src={logo} alt='sneakers logo' />
+          </div>
+          <ul className='nav__links'>
+          {links.map((link) => {
+                const {url, id, text} = link;
+                return <li className='nav__link' key={id}><a href={url}>{text}</a></li>
+              })}
+          </ul>
+       </div>
         <div className='nav__action'>
           <AiOutlineShoppingCart className='nav__cart'/>
           <img src={avatar} alt="avatar"  className='nav__avatar'/>
