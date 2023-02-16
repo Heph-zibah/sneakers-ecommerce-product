9e32908d8033e3d49e655a24f1430c535ef66156
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-16 08:41:26 +0100

    gave all tags classnames

--- src/Components/Main/Product/ProductProfile.jsx
+++ src/Components/Main/Product/ProductProfile.jsx
@@ -1,28 +1,37 @@
 import React from 'react'
 import {HiMinus,HiPlus} from 'react-icons/hi'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {productProfile} from './ProductData.js'
+import './Product.css'
 
 const ProductProfile = () => {
   return (
     <>
         <div className='product__content'>
-            <h1>SNEAKER COMPANY</h1>
-            <h2>Fall Limited Edition Sneakers</h2>
-            <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
-            <div className='product__price'>
-                <div className='product__'>
-                    <p>$125.00</p>
-                    <p>50%</p>
-                </div>
-                <p className='product__price--through'>$250.00</p>
+            <div className='product__intro'>
+                <h1>SNEAKER COMPANY</h1>
+                <h2>{productProfile.productName}</h2>
+                <p>{productProfile.productDesc}</p>
             </div>
-            <div className='content__product'>
-                <div className=''>
-                    <HiMinus className='product__add'/>
-                    <p className='product__num'>0</p>
-                    <HiPlus className='product__minus'/>
+            <div className='product__price'>
+                <p className='product__discount--price'>${productProfile.discountPrice}.00</p>
+                <p className='product__discount--percent'>{productProfile.discountPercent}%</p>
+               
+                <p className='product__original--price'>${productProfile.originalPrice}.00</p>
+            </div>
+            <div className='product__action'>
+                <div className='product__action--modify'>
+                    <HiMinus className='product__action--add'/>
+                    <p className='product__action--num'>0</p>
+                    <HiPlus className='product__aaction--minus'/>
 
                 </div>
-                <button className='product__button'>Add to cart</button>
+                <div className='product__action--btn'>
+                    <button>
+                        <AiOutlineShoppingCart/>
+                        Add to cart
+                    </button>
+                </div>
             </div>
          </div>
     </>
