92ba1acb6fdd0e3d94fa1e99d29b79ad5290ffb9
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-16 15:42:47 +0100

    added the markup

--- src/Components/Main/Product/ProductImages.jsx
+++ src/Components/Main/Product/ProductImages.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import lightbox from '../../../Assets/image-product-1.jpg'
+import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io'
 
 const ProductImages = () => {
   return (
     <>
         <div className='product__img'>
-            {/* <img src={lightbox} alt="shoe" /> */}
+            <IoIosArrowBack className='product__arrow product__arrow--left'/>
+            <img src={lightbox} alt="shoe" />
+            <IoIosArrowForward className='product__arrow product__arrow--right'/>
         </div>
     </>
   )
