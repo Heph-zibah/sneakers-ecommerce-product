8da76b178b80505b866257503a4b1ceae5170551
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-16 08:40:24 +0100

    created data file for the product profile

---
+++ src/Components/Main/Product/ProductData.js
@@ -0,0 +1,9 @@
+export const productProfile = 
+    {
+        id:1,
+        productName: 'Fall Limited Edition Sneakers',
+        productDesc: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.',
+        discountPrice: 125,
+        discountPercent:50,
+        originalPrice: 250
+    }
