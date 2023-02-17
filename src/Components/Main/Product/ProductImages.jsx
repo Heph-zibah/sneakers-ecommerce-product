8938729e2965b7532b9543981b62273c4a4d3a6c
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-17 10:04:53 +0100

    added the thumbnail images

--- src/Components/Main/Product/ProductImages.jsx
+++ src/Components/Main/Product/ProductImages.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import lightbox from '../../../Assets/image-product-1.jpg'
 import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io'
+import thumbnail1 from '../../../Assets/image-product-1-thumbnail.jpg'
+import thumbnail2 from '../../../Assets/image-product-2-thumbnail.jpg'
+import thumbnail3 from '../../../Assets/image-product-3-thumbnail.jpg'
+import thumbnail4 from '../../../Assets/image-product-4-thumbnail.jpg'
 
 const ProductImages = () => {
   return (
     <>
+      <div>
         <div className='product__img'>
             <IoIosArrowBack className='product__arrow product__arrow--left'/>
             <img src={lightbox} alt="shoe" />
             <IoIosArrowForward className='product__arrow product__arrow--right'/>
         </div>
+        <div>
+          <img src={thumbnail1} alt="show" />
+          <img src={thumbnail2} alt="show" />
+          <img src={thumbnail3} alt="show" />
+          <img src={thumbnail4} alt="show" />
+        </div>
+       </div>
     </>
   )
 }
