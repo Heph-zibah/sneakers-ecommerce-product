a6cf152072ddf7690354b747eb5a6b617c822ebb
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-18 17:27:14 +0100

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Product from "./Components/Main/Product/Product";
 function App() {
   const [itemCount, setItemCount] = useState(1)
   const [cartItems, setCartItems] = useState('')
+  
   return (
     <>
       <div className="App">
