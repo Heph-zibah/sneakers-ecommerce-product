e91a59095ee124d5240c42b68d7834a9ed8b25bd
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-18 15:52:10 +0100

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Components/HEADER/Header";
 import Product from "./Components/Main/Product/Product";
 
 function App() {
+  const [itemCount, setItemCount] = useState(1)
+  const [cartItems, setCartItems] = useState('')
   return (
     <>
       <div className="App">
-        <Header/>
+        <Header itemCount={itemCount} setItemCount={setItemCount} cartItems={cartItems}/>
         <main>
-        <Product/>
+        <Product itemCount={itemCount} setItemCount={setItemCount}/>
         </main>
       </div>
     </>
