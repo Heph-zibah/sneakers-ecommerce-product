a29dfc4f102325737718385c364c907ca5230598
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-18 15:52:39 +0100

    update file

--- src/Components/HEADER/Cart.jsx
+++ src/Components/HEADER/Cart.jsx
@@ -3,26 +3,29 @@ import thumbnail from '../../Assets/image-product-1-thumbnail.jpg'
 import {RiDeleteBin6Line} from 'react-icons/ri'
 import { productProfile } from '../Main/Product/ProductData'
 import './Cart.css'
-const Cart = () => {
+
+
+const Cart = ({cartItems}) => {
   return (
     <>
         <aside>
             <div className='cart'>
                 <div className=''>
                     <p className='cart__header'>Cart</p>
-                    <p className='cart__body'>Your cart is empty</p>
+                    {cartItems.length === 0 && <p className='cart__body'>Your cart is empty</p>}
                 </div>
+                {cartItems.length > 0 && 
                 <div className='cart__container'> 
-                    <div className='cart__items'>
-                        <img src={thumbnail} alt='shoe' />
-                        <div>
-                            <p>{productProfile.productName}</p>
-                            <p>${productProfile.discountPrice} x ${productProfile.discountPrice}</p>
-                        </div>
-                        <RiDeleteBin6Line/>
+                <div className='cart__items'>
+                    <img src={thumbnail} alt='shoe' />
+                    <div>
+                        <p>{productProfile.productName}</p>
+                        <p>${productProfile.discountPrice} x ${productProfile.discountPrice}</p>
                     </div>
-                    <button>Checkout</button>
+                    <RiDeleteBin6Line/>
                 </div>
+                <button>Checkout</button>
+            </div>}
             </div>
         </aside>
     </>
