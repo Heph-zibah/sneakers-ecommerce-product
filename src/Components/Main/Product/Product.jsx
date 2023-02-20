b7f0ae4ccad24b5bf0ba08d3b4f0ae78647655d9
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-20 14:51:53 +0100

    updaate file

--- src/Components/Main/Product/Product.jsx
+++ src/Components/Main/Product/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ProductImages from './ProductImages'
 import './Product.css'
 
--- src/Components/Main/Product/ProductProfile.jsx
+++ src/Components/Main/Product/ProductProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {HiMinus,HiPlus} from 'react-icons/hi'
 import {AiOutlineShoppingCart} from 'react-icons/ai'
 import {productProfile} from './ProductData.js'
