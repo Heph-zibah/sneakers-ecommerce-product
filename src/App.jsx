f0e6931ec6accffbf704da6e8d4d5c345304a608
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-20 14:17:32 +0100

    update state

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Product from "./Components/Main/Product/Product";
 
 function App() {
   const [itemCount, setItemCount] = useState(0)
-  const [cartItems, setCartItems] = useState('')
+  const [cartItems, setCartItems] = useState(0)
   
   return (
     <>
       <div className="App">
-        <Header itemCount={itemCount} setItemCount={setItemCount} cartItems={cartItems}/>
+        <Header cartItems={cartItems} setCartItems={setCartItems}/>
         <main>
-        <Product itemCount={itemCount} setItemCount={setItemCount}/>
+        <Product itemCount={itemCount} setItemCount={setItemCount} setCartItems={setCartItems}/>
         </main>
       </div>
     </>
