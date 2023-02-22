e4b35f2e1d88b60ee3de754b0f25489174610c37
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-22 16:42:35 +0100

    added the auth component folder

---
+++ src/Components/Auth/Auth.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const Auth = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [user, setUser] = useState()
+
+    const handleSubmit = async e => {
+
+    }
+
+    if (user) {
+        return <div>{user.name} is logged in</div>
+    }
+
+
+  return (
+    <>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">UUsername:</label>
+            <input type="text" 
+            value={username}
+            placeholder="Enter a username"
+            onChange={({target}) => 
+                setUsername(target.value)
+            }
+            />
+
+            <label htmlFor="password">Password</label>
+            <input type="password" 
+            value={password}
+            placeholder="Enter a Password"
+            onChange={({target}) => 
+                setPassword(target.value)
+            }/>
+
+            <button type='submit'>Login</button>
+
+        </form>
+    </>
+  )
+}
+
+export default Auth
