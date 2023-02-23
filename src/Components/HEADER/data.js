94e9133ae8016416eea4bf0d78243418e349437f
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-23 14:38:10 +0100

    change the fila namme from link data to data

---
+++ src/Components/HEADER/data.js
@@ -0,0 +1,47 @@
+export const links =  [
+        {
+            id:1,
+            url: '/collection',
+            text: 'collection'
+        },
+        {
+            id:2,
+            url: '/men',
+            text: 'men'
+        },
+        {
+            id:3,
+            url: '/women',
+            text: 'women'
+        },
+        {
+            id:4,
+            url: '/contact',
+            text: 'about'
+        },
+        {
+            id:5,
+            url: '/contact',
+            text: 'contact'
+        },
+    ]
+
+
+export const images  = [
+    {
+        png: ('../../Assets/image-product-1.jpg'),
+        thumbnail: ('../../Assets/image-product-1-thumbnail.jpg')
+    },
+    {
+        png: ('../../Assets/image-product-2.jpg'),
+        thumbnail: ('../../Assets/image-product-2-thumbnail.jpg')
+    },
+    {
+        png: ('../../Assets/image-product-3.jpg'),
+        thumbnail: ('../../Assets/image-product-3-thumbnail.jpg')
+    },
+    {
+        png: ('../../Assets/image-product-4.jpg'),
+        thumbnail: ('../../Assets/image-product-4-thumbnail.jpg')
+    },
+]
