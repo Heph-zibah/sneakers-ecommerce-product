a555808ada6b5f6645bb25989281b1d43538dcb8
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-25 15:12:36 +0100

    added the remaining markup

--- src/Components/Main/Product/ProductImages.jsx
+++ src/Components/Main/Product/ProductImages.jsx
@@ -10,22 +10,47 @@ const ProductImages = () => {
     <>
       <div className='product__images'>
         <div className='product__img'>
-            <IoIosArrowBack className='product__arrow product__arrow--left'/>
-            <img src={images[currentImage].png} alt="shoe" onClick={() => window.innerWidth > 800 && setLightbox(true)}/>
-            <IoIosArrowForward className='product__arrow product__arrow--right'/>
+          <IoIosArrowBack 
+          className='product__arrow product__arrow--left' 
+          onClick={() => 
+            setCurrentImage((prev) => prev === 0 ? images.length -1 : prev -1)}/>
+          <img 
+          src={images[currentImage].png} 
+          alt="shoe" 
+          onClick={() => window.innerWidth > 800 && setLightbox(true)}/>
+          <IoIosArrowForward 
+          className='product__arrow product__arrow--right' 
+          onClick={() =>
+            setCurrentImage((prev) => prev === images.length - 1 ? 0 : prev + 1)}/>
         </div>
         <div className='product__thumbnail--wrapper'>
           <div className="product__thumbnail">
-            <img src={images[0].thumbnail} alt="" />
+            <img 
+            src={images[0].thumbnail} 
+            alt="two shoes"
+            onClick={() => setCurrentImage(0)} 
+            />
           </div>
           <div className="product__thumbnail">
-            <img src={images[1].thumbnail} alt="" />
+            <img 
+            src={images[1].thumbnail} 
+            alt="two shoes on two branches with a dry branch beside" 
+            onClick={() => setCurrentImage(1)}
+            />
           </div>
           <div className="product__thumbnail">
-            <img src={images[2].thumbnail} alt="" />
+            <img 
+            src={images[2].thumbnail} 
+            alt="a shoes on two stacked stones" 
+            onClick={() => setCurrentImage(2)}
+            />
           </div>
           <div className="product__thumbnail">
-            <img src={images[3].thumbnail} alt="" />
+            <img 
+            src={images[3].thumbnail} 
+            alt="a shoe on two stacked stones" 
+            onClick={() => setCurrentImage(3)}
+            />
           </div>
         </div>
        </div>
