1875a78945f4676e518869e2aedf204d5802d57c
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-25 13:44:07 +0100

    corrected the props

--- src/Components/Main/Product/ProductImages.jsx
+++ src/Components/Main/Product/ProductImages.jsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react'
-import img from '../../../Assets/image-product-1.jpg'
 import {images} from '../../HEADER/data'
-import Lightbox from '../Lightbox'
+import Lightbox from '../Lightbox/Lightbox'
 import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io'
 
 const ProductImages = () => {
-  const [lightbox, setlightbox] = useState(false);
+  const [lightbox, setLightbox] = useState(false);
   const [currentImage, setCurrentImage] = useState(0)
   return (
     <>
       <div className='product__images'>
         <div className='product__img'>
             <IoIosArrowBack className='product__arrow product__arrow--left'/>
-            <img src={images[currentImage].png} alt="shoe" onClick={() => window.innerWidth > 800 && setlightbox(true)}/>
+            <img src={images[currentImage].png} alt="shoe" onClick={() => window.innerWidth > 800 && setLightbox(true)}/>
             <IoIosArrowForward className='product__arrow product__arrow--right'/>
         </div>
-        <div className='product__thumbnail'>
-         
+        <div className='product__thumbnail--wrapper'>
+          <div className="product__thumbnail">
+            <img src={images[0].thumbnail} alt="" />
+          </div>
+          <div className="product__thumbnail">
+            <img src={images[1].thumbnail} alt="" />
+          </div>
+          <div className="product__thumbnail">
+            <img src={images[2].thumbnail} alt="" />
+          </div>
+          <div className="product__thumbnail">
+            <img src={images[3].thumbnail} alt="" />
+          </div>
         </div>
        </div>
-       {lightbox && <Lightbox images={images} setlightbox={setlightbox} currentImage={currentImage} setCurrentImage={setCurrentImage}/>}
+       {lightbox && <Lightbox images={images} setLightbox={setLightbox} currentImage={currentImage} setCurrentImage={setCurrentImage}/>}
     </>
   )
 }
